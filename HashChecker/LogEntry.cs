472ae6d0d9b705f56a111c19736e8eca02a2eb68
UTurista <vascko@sapo.pt>
2015-09-25 18:33:19 +0100

    Added initial files

---
+++ HashChecker/LogEntry.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashChecker
+{
+    class LogEntry
+    {
+        public DateTime Date { get; private set; }
+        public string Hash { get; private set; }
+        public string Tag { get; private set; }
+        public string Nick { get; private set; }
+        public IPAddress IPAddress { get; private set; }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null)
+                return false;
+
+            LogEntry other = obj as LogEntry;
+
+            if (other == null)
+                return false;
+
+            return this.Nick.Equals(other.Nick) && this.Hash.Equals(other.Hash);
+        }
+        public override int GetHashCode()
+        {
+            return this.Hash.GetHashCode();
+        }
+
+
+
+        //[<date|yyyy-MM-dd> <hour|HH:mm:ss>] <string> <string|optional> <string> <ip>
+        public static LogEntry parse(string line)
+        {
+            LogEntry logEntry = new LogEntry();
+            string[] tokens = line.Split(' ');
+            tokens[0] += ' ' + tokens[1];
+
+            logEntry.Date = DateTime.ParseExact(tokens[0], "[yyyy-MM-dd HH:mm:ss]", CultureInfo.InvariantCulture);
+
+            int field = 2;
+            logEntry.Hash = tokens[field++];
+
+            if (tokens.Length == 6)
+            {
+                logEntry.Tag = tokens[field++];
+            }
+
+            logEntry.Nick = tokens[field++];
+
+            logEntry.IPAddress = IPAddress.Parse(tokens[field++]);
+
+            return logEntry;
+        }
+
+
+
+    }
+}
---
+++ HashChecker/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Wrong usage...");
+                Console.WriteLine(" >HashChecker.exe <server.log> <global.log>");
+                Console.WriteLine(" <server.log> is the path to the file with the hashes that we want to verify");
+                Console.WriteLine(" <global.log> is the path to the file with every hash");
+                return;
+            }
+
+            Dictionary<string, List<LogEntry>> logDups = new Dictionary<string, List<LogEntry>>();
+
+            for (int arg = 0; arg < args.Length; arg++)
+            {
+
+                try
+                {   // Open the text file using a stream reader.
+                    using (StreamReader sr = new StreamReader(args[arg]))
+                    {
+                        String line;
+
+                        while (!String.IsNullOrEmpty(line = sr.ReadLine()))
+                        {
+                            LogEntry entry = LogEntry.parse(line);
+
+                            //Only the second file will be used to compare the duplicates
+                            if (arg == 1 && logDups.ContainsKey(entry.Hash))
+                            {
+                                bool diff = true;
+                                foreach (LogEntry regEntry in logDups[entry.Hash])
+                                {
+                                    diff = regEntry.Equals(entry) ? false : diff;
+                                }
+
+                                if (diff)
+                                    logDups[entry.Hash].Add(entry);
+                            }
+                            //Only the first file will be used to populate the database
+                            else if (arg == 0)
+                            {
+                                List<LogEntry> list = new List<LogEntry>();
+                                list.Add(entry);
+                                logDups.Add(entry.Hash, list);
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("The file could not be read:");
+                    Console.WriteLine(e.Message);
+                }
+            }//End For
+
+            foreach (KeyValuePair<string, List<LogEntry>> pair in logDups)
+            {
+
+                if (pair.Value.Count > 1)
+                {
+                    Console.WriteLine("======= HASH: " + pair.Key);
+                    foreach (LogEntry entry in pair.Value)
+                    {
+                        Console.WriteLine(String.Format(">{0} from {1}", entry.Nick, entry.IPAddress));
+                    }
+
+                }
+            }//End ForEach
+        }
+    }
+}
